a7232ed88c5b315f6c4f8cd35715e3e15d17ece7
Lilian Li <lil712@hotmail.com>
2022-12-02 14:53:08 +0800

    useEffect function in app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,23 @@
+import { useState, useEffect } from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import "../styles/globals.css";
+import { supabase } from "../components/supabase"
 
 function MyApp({ Component, pageProps }) {
+  const [session, setSession ] = useState(null)
+
+  useEffect(() => {
+    setSession(supabase.auth.session())
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
  return (
    <div>
-     <Navbar/>
-     <Component {...pageProps}/>
+     <Navbar session={session}/>
+     <Component {...pageProps} session={session}/>
      <Footer />
    </div>
  );
